a7dce19b7773bd703c67c09bdec4a869a2143fcf
Jacky Cao <jcvan0116@gmail.com>
2022-04-16 12:29:56 -0700

    now there's different page navs for different screen sizes

--- components/PageNav/PageNav.jsx
+++ components/PageNav/PageNav.jsx
@@ -1,26 +1,45 @@
+import { useState, useEffect } from "react";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import styles from "./PageNav.module.scss";
+
 const PageNav = () => {
-    return (
-        <div className="page-nav">
-            <ul className="page-nav__list">
-                <li>
-                <button className="page-nav__item page-nav__service">service</button>
-                </li>
-                <li>
-                <button className="page-nav__item page-nav__testimonial">
-                    Testimonial
-                </button>
-                </li>
-                <li>
-                <button className="page-nav__item page-nav__company">Company</button>
-                </li>
-                <li>
-                <button className="page-nav__item page-nav__contact">
-                    Contact Us
-                </button>
-                </li>
-            </ul>
-        </div>
-  );
+  const hasWindow = typeof window !== "undefined";
+
+  const [windowWidth, setWindowWidth] = useState(getWindowWidth());
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  function getWindowWidth() {
+    const width = hasWindow ? window.innerWidth : null;
+    return width;
+  }
+  useEffect(() => {
+    if (hasWindow) {
+      function handleResize() {
+        setWindowWidth(getWindowWidth());
+      }
+
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, [hasWindow]);
+  console.log(windowWidth);
+  return mounted && windowWidth >= 768 ? (
+    <div className={[styles["page-nav"], styles["page-nav--large"]].join(" ")}>
+      <ul className={styles["page-nav__list"]}>
+        <li className={[styles["page-nav__button"], styles["page-nav__service"]].join(" ")}>service</li>
+        <li className={[styles["page-nav__button"], styles["page-nav__testimonial"]].join(" ")}>Testimonial</li>
+        <li className={[styles["page-nav__button"], styles["page-nav__company"]].join(" ")}>Company</li>
+        <li className={[styles["page-nav__button"], styles["page-nav__contact"]].join(" ")}>Contact Us</li>
+      </ul>
+    </div>
+  ) : mounted ? (
+    <div className={[styles["page-nav"], styles["page-nav--small"]].join(" ")}>
+      <MenuRoundedIcon fontSize="large" className={styles["page-nav__burger"]} />
+    </div>
+  ) : null;
 };
 
 export default PageNav;
