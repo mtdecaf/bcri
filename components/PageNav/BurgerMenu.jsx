23fe2f7e239a01d0aec640395804901b83c14662
Jacky Cao <jcvan0116@gmail.com>
2022-04-23 17:19:38 -0700

    small sized page nav functional

--- components/PageNav/BurgerMenu.jsx
+++ components/PageNav/BurgerMenu.jsx
@@ -9,8 +9,11 @@ const BurgerMenu = ({ isOpen, setIsOpen }) => {
         <ClearRoundedIcon className={styles["burger-menu__close"]} style={{ fontSize: 50 }} onClick={() => setIsOpen(false)} />
         <div className={styles["burger-menu__list"]}>
             <a className={styles["burger-menu__button"]} href="">Service</a>
+            <hr className={styles["burger-menu__divider"]}/>
             <a className={styles["burger-menu__button"]} href="">Testimonial</a>
+            <hr className={styles["burger-menu__divider"]}/>
             <a className={styles["burger-menu__button"]} href="">Company</a>
+            <hr className={styles["burger-menu__divider"]}/>
             <a className={styles["burger-menu__button"]} href="">Contact Us</a>
         </div>
       </div>
--- components/PageNav/PageNav.jsx
+++ components/PageNav/PageNav.jsx
@@ -36,27 +36,35 @@ const PageNav = () => {
   };
   return mounted && windowWidth >= 768 ? (
     <div className={[styles["page-nav"], styles["page-nav--large"]].join(" ")}>
-      <ul className={styles["page-nav__list"]}>
-        <li className={[styles["page-nav__button"], styles["page-nav__service"]].join(" ")}>service</li>
-        <li className={[styles["page-nav__button"], styles["page-nav__testimonial"]].join(" ")}>Testimonial</li>
-        <li className={[styles["page-nav__button"], styles["page-nav__company"]].join(" ")}>Company</li>
-        <li className={[styles["page-nav__button"], styles["page-nav__contact"]].join(" ")}>Contact Us</li>
-      </ul>
+      <div className={styles["page-nav__list"]}>
+        <a className={[styles["page-nav__button"], styles["page-nav__service"]].join(" ")} href="">Service</a>
+        <a className={[styles["page-nav__button"], styles["page-nav__testimonial"]].join(" ")} href="">Testimonial</a>
+        <a className={[styles["page-nav__button"], styles["page-nav__company"]].join(" ")} href="">Company</a>
+        <a className={[styles["page-nav__button"], styles["page-nav__contact"]].join(" ")} href="">Contact Us</a>
+      </div>
     </div>
   ) : mounted ? (
     <>
     <BurgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
+    {windowWidth <= 425 ? 
     <div className={styles["page-nav__contact-bar"]}>
       <div className={styles["page-nav__contact-bar__info-wrap"]}>
         <FiPhone className={styles["page-nav__contact-bar__phone-icon"]} />
         <p className={styles["page-nav__contact-bar__info"]}><a href="tel:+16045392510">(604)-539-2510</a></p>
       </div>
-    </div>
+    </div> : null}
     <div className={[styles["page-nav"], styles["page-nav--small"]].join(" ")}>
       <div className={styles["page-nav__logo-wrap"]}>
         <Image className={styles["page-nav__logo"]} width="100%" height="40%" objectFit="contain" src={bcriLogo} alt="BCRI logo" />
       </div>
-      <MenuRoundedIcon className={styles["page-nav__burger"]} fontSize="large" onClick={() => handleClick()} />
+      <div className={styles["page-nav__right-wrap"]}>
+        {windowWidth > 425 ?
+        <div className={styles["page-nav__contact-button__wrap"]}>
+          <FiPhone className={styles["page-nav__contact-button__phone-icon"]} />
+          <p className={styles["page-nav__contact-button"]}><a href="tel:+16045392510">(604)-539-2510</a></p>
+        </div> : null}
+        <MenuRoundedIcon className={styles["page-nav__burger"]} fontSize="large" onClick={() => handleClick()} />
+      </div>
     </div>
     </>
   ) : null;
