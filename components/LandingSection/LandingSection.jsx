11fd3a7645c0f54ce25ec12b56c86ed91cb0a0cd
Jacky Cao <jcvan0116@gmail.com>
2022-04-27 09:03:23 -0700

    fixed desktop side padding

---
+++ components/LandingSection/LandingSection.jsx
@@ -0,0 +1,11 @@
+import styles from "./LandingSection.module.scss";
+
+const LandingSection = () => {
+    return (
+        <div className="landing-section">
+            home
+        </div>
+    )
+}
+
+export default LandingSection;
