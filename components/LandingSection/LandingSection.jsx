288a65f42e680b9fbcb84a3c90ce77cb24f6cc99
Jacky Cao <jcvan0116@gmail.com>
2022-04-30 14:39:23 -0700

    mobile landing page

--- components/LandingSection/LandingSection.jsx
+++ components/LandingSection/LandingSection.jsx
@@ -1,11 +1,15 @@
 import styles from "./LandingSection.module.scss";
-import classNames from "classnames";
+import classNames from "classnames/bind";
 let cx = classNames.bind(styles);
 
 const LandingSection = () => {
     return (
-        <div className={cx("landing-section")}>
-            <h1></h1>
+        <div className={cx("landing-section")} id="landing-section">
+            <h1 className={cx("landing-section__title")}>The <br></br> Roof <br></br> Inspections <br></br> You Can Count On</h1>
+            <div className={cx("landing-section__button-wrap")}>
+                <button className={cx("landing-section__button", "landing-section__button--primary", "landing-section__button-contact")} href="">CONTACT US</button>
+                <button className={cx("landing-section__button", "landing-section__button--sub", "landing-section__button-service")} href="">OUR SERVICES</button>
+            </div>
         </div>
     )
 }
