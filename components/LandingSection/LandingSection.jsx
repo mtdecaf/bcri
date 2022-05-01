2064a936850258d25475395e05dfcd43ad34226f
Jacky Cao <jcvan0116@gmail.com>
2022-04-30 21:12:40 -0700

    landing page added and styled

--- components/LandingSection/LandingSection.jsx
+++ components/LandingSection/LandingSection.jsx
@@ -1,14 +1,45 @@
+import { useState, useEffect } from "react";
+
 import styles from "./LandingSection.module.scss";
 import classNames from "classnames/bind";
 let cx = classNames.bind(styles);
 
 const LandingSection = () => {
-    return (
+    const hasWindow = typeof window !== "undefined";
+
+    const [mounted, setMounted] = useState(false);
+    const [windowWidth, setWindowWidth] = useState(getWindowWidth());
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    function getWindowWidth() {
+        const width = hasWindow ? window.innerWidth : null;
+        return width;
+      }
+      useEffect(() => {
+        if (hasWindow) {
+          function handleResize() {
+            setWindowWidth(getWindowWidth());
+          }
+    
+          window.addEventListener("resize", handleResize);
+          return () => window.removeEventListener("resize", handleResize);
+        }
+    }, [hasWindow]);
+
+    return mounted && (
         <div className={cx("landing-section")} id="landing-section">
-            <h1 className={cx("landing-section__title")}>The <br></br> Roof <br></br> Inspections <br></br> You Can Count On</h1>
-            <div className={cx("landing-section__button-wrap")}>
-                <button className={cx("landing-section__button", "landing-section__button--primary", "landing-section__button-contact")} href="">CONTACT US</button>
-                <button className={cx("landing-section__button", "landing-section__button--sub", "landing-section__button-service")} href="">OUR SERVICES</button>
+            <div className={cx("landing-section__inner-wrap")}>
+                {windowWidth >= 768 ?
+                    <h1 className={cx("landing-section__title")}>The Roof Inspections <br></br> You Can Count On</h1>
+                    : <h1 className={cx("landing-section__title")}>The <br></br> Roof <br></br> Inspections <br></br> You Can Count On</h1>
+                }
+                <div className={cx("landing-section__button-wrap")}>
+                    <button className={cx("landing-section__button", "landing-section__button--primary", "landing-section__button-contact")} href="">CONTACT US</button>
+                    <button className={cx("landing-section__button", "landing-section__button--sub", "landing-section__button-service")} href="">OUR SERVICES</button>
+                </div>
             </div>
         </div>
     )
