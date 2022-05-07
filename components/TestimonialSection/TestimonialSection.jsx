8621ec92e18535d1c22d081f6a312c6fef02c808
Jacky Cao <jcvan0116@gmail.com>
2022-05-07 16:02:15 -0700

    added depth to the UI

--- components/TestimonialSection/TestimonialSection.jsx
+++ components/TestimonialSection/TestimonialSection.jsx
@@ -6,7 +6,7 @@ const TestimonialSection = () => {
     return(
         <section className={cx("testimonial-section")} id="testimonial-section">
             <div className={cx("testimonial-section__title-wrap")}>
-                <h1 className={cx("testimonial-section__title")}>What our clients say</h1>
+                <h1 className={cx("testimonial-section__title")}>Testimonials</h1>
             </div>
         </section>
     )
