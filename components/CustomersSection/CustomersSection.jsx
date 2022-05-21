101b046f0ee30eb0cd49e5e26709a5ccc4a2b297
Jacky Cao <jcvan0116@gmail.com>
2022-05-20 17:20:58 -0700

    styled trusted by section

--- components/CustomersSection/CustomersSection.jsx
+++ components/CustomersSection/CustomersSection.jsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import { useRef, useEffect, useState } from "react";
 
 import styles from "./CustomersSection.module.scss";
 import classNames from "classnames/bind";
@@ -7,20 +7,25 @@ let cx = classNames.bind(styles);
 const CustomersSection = () => {
   const customersNum = 29;
   const customers = [];
-  for (let i = 1; i <= customersNum; i++) {
-    console.log("hi")
-    customers.push(
-      <div className={cx("customers-section__logo-container")} key={i}>
-        <Image 
-          className={cx("customers-section__logo")}
-          src={`/customers-logo/customer${i}.png`}
-          alt={`customer${i}`}
-          width="100%"
-          height="48px"
-        />
-      </div>
-    );
-  }
+  const customersRef = useRef(null);
+
+  const createCustomer = () => {
+    for (let i = 1; i <= customersNum; i++) {
+      customers.push(
+        <div
+        className={cx("customers-section__logo-container")}
+        key={i}
+        >
+          <img
+            className={cx("customers-section__logo")}
+            ref={customersRef}
+            src={`/customers-logo/customer${i}.png`}
+            alt={`customer${i}`}
+            />
+        </div>
+      );
+    }
+  };
 
   return (
     <div className={cx("customers-section")}>
@@ -29,6 +34,7 @@ const CustomersSection = () => {
       </div>
       {/* create an image for every file in customers-logo folder */}
       <div className={cx("customers-section__logos-wrap")}>
+        {createCustomer()}
         {customers}
       </div>
     </div>
