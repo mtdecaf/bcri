7a7d63ab63f1350ee88b7c0fd93dbae3283ea517
Jacky Cao <jcvan0116@gmail.com>
2022-05-28 10:23:07 -0700

    code refactor

--- components/TestimonialSection/TestimonialSection.jsx
+++ components/TestimonialSection/TestimonialSection.jsx
@@ -1,38 +1,20 @@
 import TestimonialCard from "./components/TestimonialCard";
+import tesimonials from "../../data/testimonialsData.json";
 import Link from "next/link";
 
 import styles from "./TestimonialSection.module.scss";
 import classNames from 'classnames/bind';
 let cx = classNames.bind(styles);
 
-const testimonialData = [
-    {
-        name: "John Doe",
-        rating: 4.5,
-        company: "Google",
-        testimonial: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget nisl id libero consectetur fermentum. Nulla euismod, urna eu tempor consectetur, nisi nisl aliquam nunc, euismod aliquam nisl nisi euismod.",
-    },
-    {
-        name: "John Smith",
-        rating: 5,
-        company: "Apple",
-        testimonial: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget nisl id libero consectetur fermentum. Nulla euismod, urna eu tempor consectetur, nisi nisl aliquam nunc, euismod aliquam nisl nisi euismod.",
-    },
-    {
-        name: "Shirley Jones",
-        rating: 5,
-        company: "Facebook",
-        testimonial: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget nisl id libero consectetur fermentum. Nulla euismod, urna eu tempor consectetur, nisi nisl aliquam nunc, euismod aliquam nisl nisi euismod.",
-    }
-]
 const TestimonialSection = ({mounted, windowWidth}) => {
+    const { testimonialsData } = tesimonials;
     return(
         <section className={cx("testimonial-section")} id="testimonial-section">
             <div className={cx("testimonial-section__title-wrap")}>
                 <h2 className={cx("testimonial-section__title")}>Why People Choose Us</h2>
             </div>
             <div className={cx("testimonial-section__cards-wrap")}>
-                {testimonialData.map((data, key) => <TestimonialCard testimonialData={data} key={key} index={key} mounted={mounted} windowWidth={windowWidth} />)}
+                {testimonialsData.map((data, key) => <TestimonialCard testimonialData={data} key={key} index={key} mounted={mounted} windowWidth={windowWidth} />)}
             </div>
             <div className={cx("testimonial-section__button-wrap")}>
                 <Link href="/testimonies"><a className={cx("testimonial-section__button")}>SEE MORE TESTIMONIALS</a></Link>
