f1e647ed2616e4d6a528ed908aa5fa6306e2ec17
Jacky Cao <jcvan0116@gmail.com>
2022-06-13 22:26:20 -0700

    added testimonials section page

---
+++ components/TestimonialsPageComponents/TestimonialsSection/TestimonialsSection.jsx
@@ -0,0 +1,21 @@
+import Slider from "react-slick";
+
+import styles from "./TestimonialSection.module.scss";
+import classNames from "classnames/bind";
+let cx = classNames.bind(styles);
+
+const TestimonialSection = () => {
+    return (
+        <div className={cx("testimonials-section")}>
+            <div className={cx("testimonials-section__carousel-wrap")}>
+                <Slider
+                    className={cx("testimonials-section__carousel")}
+                >
+                    
+                </Slider>
+            </div>
+        </div>
+    );
+}
+
+export default TestimonialSection;
