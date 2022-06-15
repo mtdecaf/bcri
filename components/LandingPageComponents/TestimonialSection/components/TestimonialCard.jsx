9117b797943895f6c872eed27ca249362ae14fb6
Jacky Cao <jcvan0116@gmail.com>
2022-06-14 18:13:28 -0700

    added proper fading border to testimonials cards

--- components/LandingPageComponents/TestimonialSection/components/TestimonialCard.jsx
+++ components/LandingPageComponents/TestimonialSection/components/TestimonialCard.jsx
@@ -1,4 +1,5 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
+import { useEffectOnce } from "/custom_hooks/useEffectOnce";
 import { Rating } from "@mui/material";
 import { BsChevronCompactDown, BsChevronCompactUp } from "react-icons/bs";
 import ScrollAnimation from "react-animate-on-scroll";
@@ -9,6 +10,42 @@ let cx = classNames.bind(styles);
 
 const TestimonialCard = ({ testimonialData, index, mounted, windowWidth }) => {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [isOverflowed, setIsOverflowed] = useState(false);
+  const [textHeight, setTextHeight] = useState(null);
+  const [textContainerHeight, setTextContainerHeight] = useState(null);
+
+  const testimonialTextRef = useRef();
+  const testimonialTextContainerRef = useRef();
+  const { name, company, testimonial, rating } = testimonialData;
+
+  useEffect(() => {
+    if (mounted) {
+      setTextHeight(testimonialTextRef.current.offsetHeight);
+      setTextContainerHeight(testimonialTextContainerRef.current.offsetHeight);
+    }
+  }, [mounted]);
+
+  useEffect(() => {
+    if (mounted) {
+      textHeight &&
+        textContainerHeight &&
+        setIsOverflowed(textHeight > textContainerHeight);
+    }
+  }, [textHeight, textContainerHeight]);
+
+  useEffect(() => {
+    // if the testimonial isn't expanded and is overflowed, add a class to the text box
+    if (mounted && !isExpanded && isOverflowed) {
+      console.log("run")
+      testimonialTextRef.current.classList.add(
+        cx("testimonial-card__content--fade")
+      );
+    } else if (mounted){
+      testimonialTextRef.current.classList.remove(
+        cx("testimonial-card__content--fade")
+      );
+    }
+  }, [mounted, isExpanded, isOverflowed]);
 
   const testimonialCardStyle = {
     justifyContent:
@@ -19,18 +56,19 @@ const TestimonialCard = ({ testimonialData, index, mounted, windowWidth }) => {
         : "flex-end",
     height: "100%",
     maxHeight: isExpanded ? "100rem" : "441px",
-    transition: "all 0.75s",
+    transition: "all 1s",
   };
 
-  const testimonialCardTextStyle = {
-    height: isExpanded ? "" : "100%",
+  const testimonialCardTextWrapStyle = {
+    height: "fit-content",
     maxHeight: isExpanded ? "100%" : "15rem",
-    transition: "all 0.75s",
+    transition: "all 1s",
   };
 
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
   };
+  console.log(isOverflowed);
 
   return (
     mounted && (
@@ -43,40 +81,40 @@ const TestimonialCard = ({ testimonialData, index, mounted, windowWidth }) => {
         >
           <Rating
             className={cx("testimonial-card__rating")}
-            value={testimonialData.rating}
+            value={rating}
             precision={0.5}
             readOnly
           />
           <div
             className={cx("testimonial-card__content-wrap")}
-            style={testimonialCardTextStyle}
+            style={testimonialCardTextWrapStyle}
+            ref={testimonialTextContainerRef}
           >
-            <p className={cx("testimonial-card__content")}>
-              {testimonialData.testimonial}
+            <p
+              className={cx("testimonial-card__content")}
+              ref={testimonialTextRef}
+            >
+              {testimonial}
             </p>
           </div>
-          {isExpanded ? (
+          {isOverflowed && isExpanded ? (
             <BsChevronCompactUp
               className={cx("testimonial-card__content-expand")}
               onClick={toggleExpand}
               size={24}
             />
-          ) : (
+          ) : isOverflowed ? (
             <BsChevronCompactDown
               className={cx("testimonial-card__content-expand")}
               onClick={toggleExpand}
               size={24}
             />
-          )}
+          ) : null}
           <div className={cx("testimonial-card__name-wrap")}>
-            <p className={cx("testimonial-card__name")}>
-              {testimonialData.name.toUpperCase()}
-            </p>
+            <p className={cx("testimonial-card__name")}>{name.toUpperCase()}</p>
           </div>
           <div className={cx("testimonial-card__company-wrap")}>
-            <p className={cx("testimonial-card__company")}>
-              {testimonialData.company}
-            </p>
+            <p className={cx("testimonial-card__company")}>{company}</p>
           </div>
         </ScrollAnimation>
       </div>
