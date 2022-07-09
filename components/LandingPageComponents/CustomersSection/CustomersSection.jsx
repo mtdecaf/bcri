b635486a0c83162354304bca62415c1a255d2e47
Jacky Cao <jcvan0116@gmail.com>
2022-07-09 12:30:39 -0700

    updated customers page

--- components/LandingPageComponents/CustomersSection/CustomersSection.jsx
+++ components/LandingPageComponents/CustomersSection/CustomersSection.jsx
@@ -11,43 +11,42 @@ const PAUSED = 0;
 const RUNNING = 1;
 
 const CustomersSection = ({ mounted }) => {
-  const customersNum = 21;
+  const customersNum = 24;
   const [customers, setCustomers] = useState([]);
-    const createCustomerIndex = () => {
-      // create a list of customers
-      for (let i = 1; i <= customersNum; i++) {
-        setCustomers((customers) => [...customers, i]);
-      }
-    };
-  
-    useEffectOnce(() => {
-      createCustomerIndex();
-    }, []);
+  const createCustomerIndex = () => {
+    // create a list of customers
+    for (let i = 1; i <= customersNum; i++) {
+      setCustomers((customers) => [...customers, i]);
+    }
+  };
+
+  useEffectOnce(() => {
+    createCustomerIndex();
+  }, []);
 
   return (
     mounted && (
       <div className={cx("customers-section")}>
-        {/* create an image for every file in customers-logo folder */}
-        <div className={cx("customers-section__logos-wrap")}>
-          <div
-            className={cx("customers-section__logos-marquee")}
-          >
-            <div className={cx("customers-section__logos-row-one")}>
-              <div className={cx("customers-section__logos-row-inner-wrap")}>
-                {customers.map((customer) => (
-                  <div
-                    className={cx("customers-section__logo-container")}
-                    key={customer}
-                  >
-                    <img
-                      className={cx("customers-section__logo")}
-                      src={`/customers-logo/customer${customer}.png`}
-                      alt={`customer${customer}`}
-                    />
-                  </div>
-                ))}
-              </div>
+        <div className={cx("customers-section__inner-wrap")}>
+          <div className={cx("customers-section__desc-outter-wrap")}>
+            <div className={cx("customers-section__desc-container")}>
+              <h3 className={cx("customers-section__desc")}>PROPERTIES WHO TRUST BCRI</h3>
             </div>
+          </div>
+          <div className={cx("customers-section__grid")}>
+            {customers.map((customer) => (
+              <div
+                className={cx("customers-section__logo-wrap")}
+                key={customer}
+              >
+                <img
+                  className={cx("customers-section__logo")}
+                  src={`/customers-logo/customer${customer}.png`}
+                  alt={`customer${customer}`}
+                  onError={(event) => event.target.style.display = 'none'}
+                />
+              </div>
+            ))}
           </div>
         </div>
       </div>
