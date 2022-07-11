e6f48dcd622f9e72b562794692c18fe5ddcb95e8
Jacky Cao <jcvan0116@gmail.com>
2022-07-11 15:16:22 -0700

    our services done

--- components/LandingPageComponents/ServiceDetailsSection/ServiceDetailsSection.jsx
+++ components/LandingPageComponents/ServiceDetailsSection/ServiceDetailsSection.jsx
@@ -1,17 +1,22 @@
 import Description from "./components/Description";
 import ServiceTab from "./components/ServiceTab";
 
+import services from "../../../data/servicesData.json";
+
 import styles from "./ServiceDetailsSection.module.scss";
 import classNames from "classnames/bind";
 let cx = classNames.bind(styles);
 
 const ServiceDetailsSection = () => {
+  const { servicesData } = services;
   return (
     <section className={cx("service-details-section")}>
       <div className={cx("service-details-section__inner-wrap")}>
         <Description />
         <div className={cx("service-details-section__tab-wrap")}>
-            <ServiceTab />
+          {servicesData.map((data, key) => (
+            <ServiceTab servicesData={data} key={key} />
+          ))}
         </div>
       </div>
     </section>
--- components/LandingPageComponents/ServiceDetailsSection/components/ServiceTab.jsx
+++ components/LandingPageComponents/ServiceDetailsSection/components/ServiceTab.jsx
@@ -1,30 +1,22 @@
-import {
-  Accordion,
-  AccordionSummary,
-  AccordionActions,
-  AccordionDetails,
-} from "@mui/material";
-import AddIcon from '@mui/icons-material/Add';
+import { Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
 
 import styles from "./ServiceTab.module.scss";
 import classNames from "classnames/bind";
 let cx = classNames.bind(styles);
 
-const ServiceTab = ({}) => {
+const ServiceTab = ({ servicesData }) => {
+
   return (
     <Accordion className={cx("service-tab")} disableGutters>
       <AccordionSummary
+        className={cx("service-tab__summary")}
         expandIcon={<AddIcon />}
-        aria-controls="panel1a-content"
-        id="panel1a-header"
       >
-        <p>Hi</p>
+        <p className={cx("service-tab__title")}>{servicesData.title}</p>
       </AccordionSummary>
-      <AccordionDetails>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-        </p>
+      <AccordionDetails className={cx("service-tab__content")}>
+        <p>{servicesData.description}</p>
       </AccordionDetails>
     </Accordion>
   );
