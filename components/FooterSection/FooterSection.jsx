3edc8867126f22501fa860b2513b3cf949f9a7a7
Jacky Cao <jcvan0116@gmail.com>
2022-07-13 09:44:25 -0700

    fixed line height

--- components/FooterSection/FooterSection.jsx
+++ components/FooterSection/FooterSection.jsx
@@ -36,7 +36,7 @@ const FooterSection = () => {
           <img className={cx("footer-section__partners")} src="/partners/partner2.png" alt="International Institute of Building Enclosure Consultant" />
           <img className={cx("footer-section__partners")} src="/partners/partner3.png" alt="" />
         </div>
-        <div className={cx("footer-section__contact-wrap")}>
+        <div className={cx("footer-section__contact-wrap", "footer-section__contact-content-wrap")}>
           <h3
             className={cx(
               "footer-section__contact-title",
@@ -45,15 +45,15 @@ const FooterSection = () => {
           >
             Contact Us
           </h3>
-          <div className={cx("footer-section__contact-phone-wrap")}>
+          <div className={cx("footer-section__contact-phone-wrap", "footer-section__contact-content-wrap")}>
             <a
-              className={cx("footer-section__contact-phone-link")}
+              className={cx("footer-section__contact-phone-link", "footer-section__contact-content-wrap")}
               href="tel:+16045392510"
             >
               (604)-539-2510
             </a>
           </div>
-          <div className={cx("footer-section__contact-email-wrap")}>
+          <div className={cx("footer-section__contact-email-wrap", "footer-section__contact-content-wrap")}>
             <a
               className={cx("footer-section__contact-email-link")}
               href="mailto: info@bcroofinspections.com"
@@ -61,7 +61,7 @@ const FooterSection = () => {
               info@bcroofinspections.com
             </a>
           </div>
-          <div className={cx("footer-section__contact-socials-wrap")}>
+          <div className={cx("footer-section__contact-socials-wrap", "footer-section__contact-content-wrap")}>
             <a className={cx("footer-section__contact-socials-link")} href="">
               <BsLinkedin size={32} />
             </a>
