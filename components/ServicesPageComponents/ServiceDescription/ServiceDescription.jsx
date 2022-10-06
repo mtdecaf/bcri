512b6a185474109ad1b2f980a5699bd0e044cedd
Jacky Cao <jcvan0116@gmail.com>
2022-10-05 20:55:55 -0700

    added services togle

--- components/ServicesPageComponents/ServiceDescription/ServiceDescription.jsx
+++ components/ServicesPageComponents/ServiceDescription/ServiceDescription.jsx
@@ -1,8 +1,12 @@
+import { Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+
 import styles from "./ServiceDescription.module.scss";
 import classNames from "classnames/bind";
 let cx = classNames.bind(styles);
 
 const ServiceDescription = ({ serviceSelectedData }) => {
+  console.log(JSON.stringify(serviceSelectedData?.description)?.slice(1, -1));
   return (
     serviceSelectedData && (
       <div className={cx("service-description")}>
@@ -12,6 +16,7 @@ const ServiceDescription = ({ serviceSelectedData }) => {
           </h2>
           <p className={cx("service-description__content")}>
             {JSON.stringify(serviceSelectedData?.description)
+              .slice(1, -1)
               .split("\\n")
               .map((line, key) => (
                 <span key={key}>
@@ -20,6 +25,58 @@ const ServiceDescription = ({ serviceSelectedData }) => {
                 </span>
               ))}
           </p>
+          {serviceSelectedData?.ul ? (
+            <div className={cx("service-description__accordion-wrap")}>
+              <Accordion
+                className={cx("service-description__accordion")}
+                disableGutters
+              >
+                <AccordionSummary
+                  className={cx("service-description__accordion-summary")}
+                  expandIcon={<AddIcon />}
+                >
+                  <p className={cx("service-description__accordion-title")}>
+                    {serviceSelectedData.ul.summary}
+                  </p>
+                </AccordionSummary>
+                <AccordionDetails
+                  className={cx("service-description__accordion-content")}
+                >
+                  <ul className={cx("service-description__accordion-content")}>
+                    {serviceSelectedData.ul.list.map((line, key) => (
+                      <li key={key}>{line}</li>
+                    ))}
+                  </ul>
+                </AccordionDetails>
+              </Accordion>
+            </div>
+          ) : null}
+          {serviceSelectedData?.ol ? (
+            <div className={cx("service-description__accordion-wrap")}>
+              <Accordion
+                className={cx("service-description__accordion")}
+                disableGutters
+              >
+                <AccordionSummary
+                  className={cx("service-description__accordion-summary")}
+                  expandIcon={<AddIcon />}
+                >
+                  <p className={cx("service-description__accordion-title")}>
+                    {serviceSelectedData.ol.summary}
+                  </p>
+                </AccordionSummary>
+                <AccordionDetails
+                  className={cx("service-description__accordion-content")}
+                >
+                  <ol className={cx("service-description__accordion-content")}>
+                    {serviceSelectedData.ol.list.map((line, key) => (
+                      <li key={key}>{line}</li>
+                    ))}
+                  </ol>
+                </AccordionDetails>
+              </Accordion>
+            </div>
+          ) : null}
         </div>
       </div>
     )
